411de3ef40d3f2ea0a0c890b3806b5bb75651ca9
Leo Urzua <d.leonardo.urzua@gmail.com>
2020-08-12 13:58:54 -0500

    interfaces: basic practice

--- c-interfaces/main.go
+++ c-interfaces/main.go
@@ -2,24 +2,34 @@ package main
 
 import "fmt"
 
-type englishBot struct {}
+type bot interface {
+	getGreeting() string
+}
 
+type englishBot struct {}
 type spanishBot struct {}
 
 func main()  {
 	eb := englishBot{}
 	sb := spanishBot{}
 
-	printGreetingEnglish(eb)
-	printGreetingSpanish(sb)
+	//printGreetingEnglish(eb)
+	//printGreetingSpanish(sb)
+
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
-func printGreetingEnglish(eb englishBot){
-	fmt.Println(eb.getGreeting())
-}
+//func printGreetingEnglish(eb englishBot){
+//	fmt.Println(eb.getGreeting())
+//}
+//
+//func printGreetingSpanish(sb spanishBot){
+//	fmt.Println(sb.getGreeting())
+//}
 
-func printGreetingSpanish(sb spanishBot){
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot){
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string {
