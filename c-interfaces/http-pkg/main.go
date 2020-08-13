477217b10a6f0332b75feeb424c3f49d0e9c6131
Leo Urzua <d.leonardo.urzua@gmail.com>
2020-08-13 14:07:50 -0500

    custom writer

--- c-interfaces/http-pkg/main.go
+++ c-interfaces/http-pkg/main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 )
+
+type logWriter struct {}
 
 func main(){
 	resp, err := http.Get("http://google.com")
@@ -17,5 +18,15 @@ func main(){
 	//resp.Body.Read(bs)
 	//fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	//io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error){ // this guy is implementing the Write interface since it is following its structure
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
